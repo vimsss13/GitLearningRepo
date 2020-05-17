233e0ecbea1af49ecbcdba1493a1936e9f935e72
Vimal Srivastava <vmlsrivastava08@gmail.com>
2020-05-17 09:04:52 -0700

    loops unerstood

---
+++ ForLoopsVimal.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class ForLoopsVimal
+{
+	public static void main(String[] args)
+	{
+	ArrayList<String> names=new ArrayList<>(Arrays.asList("Vimal","Parul","Kumar Prashant"));
+	
+	for(String name:names)
+	{
+		System.out.println(name);
+	}
+	
+	}
+}
